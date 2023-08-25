df5f836d68dde31bc393cc72db626eb9560913ba
Martin Erhart <merhart@google.com>
2023-08-25 12:45:17 +0000

    [mlir][bufferization] BufferDeallocationOpInterface: support custom ownership update logic
    Add a method to the BufferDeallocationOpInterface that allows operations to
    implement the interface and provide custom logic to compute the ownership
    indicators of values it defines. As a demonstrating example, this new method is
    implemented by the `arith.select` operation.

--- mlir/include/mlir/InitAllDialects.h
+++ mlir/include/mlir/InitAllDialects.h
@@ -21,6 +21,7 @@
 #include "mlir/Dialect/Affine/TransformOps/AffineTransformOps.h"
 #include "mlir/Dialect/Arith/IR/Arith.h"
 #include "mlir/Dialect/Arith/IR/ValueBoundsOpInterfaceImpl.h"
+#include "mlir/Dialect/Arith/Transforms/BufferDeallocationOpInterfaceImpl.h"
 #include "mlir/Dialect/Arith/Transforms/BufferizableOpInterfaceImpl.h"
 #include "mlir/Dialect/ArmNeon/ArmNeonDialect.h"
 #include "mlir/Dialect/ArmSME/IR/ArmSME.h"
@@ -153,6 +154,7 @@ inline void registerAllDialects(DialectRegistry &registry) {
 
   // Register all external models.
   affine::registerValueBoundsOpInterfaceExternalModels(registry);
+  arith::registerBufferDeallocationOpInterfaceExternalModels(registry);
   arith::registerBufferizableOpInterfaceExternalModels(registry);
   arith::registerValueBoundsOpInterfaceExternalModels(registry);
   bufferization::func_ext::registerBufferizableOpInterfaceExternalModels(
